4b319fdb0051cb051f0423f3288032b4c7a3d1b8
Adam-pw <adampithewan@gmail.com>
2023-12-20 19:35:36 +0530

    Feat: Added strips and Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,81 @@
-import Image from "next/image";
+import Navbar from "@/components/reusables/Navbar";
 
 export default function Home() {
-  return <div className="">HackTvarit</div>;
+  return (
+    <div className="">
+      <Navbar />
+      <div className="">
+        <div className="strips relative text-5xl my-20 p-2 font-extrabold">
+          <div className="flex justify-between">
+            <div className="">WEB DEVELOPMENT</div>
+            <div className="">WEB3</div>
+            <div className="">DATA SCIENCE</div>
+            <div className="">BLOCKCHAIN</div>
+          </div>
+        </div>
+        <div className="strips-reverse text-5xl -translate-y-40 -rotate-3 my-20 p-2 font-extrabold">
+          <div className="flex justify-between overflow-hidden">
+            <div className="">WEB DEVELOPMENT</div>
+            <div className="">WEB3</div>
+            <div className="">DATA SCIENCE</div>
+            <div className="">BLOCKCHAIN</div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="strips relative text-5xl my-20 p-2 font-extrabold">
+          <div className="flex justify-between">
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+          </div>
+        </div>
+        <div className="strips-reverse text-5xl -translate-y-40 -rotate-3 my-20 p-2 font-extrabold">
+          <div className="flex justify-between overflow-hidden">
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+            <div className="">TRACKS</div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="strips relative text-5xl my-20 p-2 font-extrabold">
+          <div className="flex justify-between">
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+          </div>
+        </div>
+        <div className="strips-reverse text-5xl -translate-y-40 -rotate-3 my-20 p-2 font-extrabold">
+          <div className="flex justify-between overflow-hidden">
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+            <div className="">PRIZES</div>
+          </div>
+        </div>
+      </div>
+      <div className="">
+        <div className="strips relative text-5xl my-20 p-2 font-extrabold">
+          <div className="flex justify-between">
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+          </div>
+        </div>
+        <div className="strips-reverse text-5xl -translate-y-40 -rotate-3 my-20 p-2 font-extrabold">
+          <div className="flex justify-between overflow-hidden">
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+            <div className="">SPONSOR</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
