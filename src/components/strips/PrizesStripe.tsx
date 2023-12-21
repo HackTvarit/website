c73e26bae2df371df3cbbe201f47b59b38a9a45c
Adam-pw <adampithewan@gmail.com>
2023-12-21 20:09:45 +0530

    Feat: Added Marquee in the strips

--- src/components/strips/PrizesStripe.tsx
+++ src/components/strips/PrizesStripe.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const PrizesStripe = () => {
   return (
     <div className="relative my-20">
       <div className="strips relative text-5xl p-2 font-extrabold">
-        <div className="flex justify-between">
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-        </div>
+        <Marquee className="flex justify-between" direction="right">
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+        </Marquee>
       </div>
       <div className="strips-reverse absolute top-0 w-full left-0 text-5xl -rotate-3 p-2 font-extrabold">
-        <div className="flex justify-between overflow-hidden">
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-          <div className="">PRIZES</div>
-        </div>
+        <Marquee className="flex justify-between">
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+          <div className="px-40">PRIZES</div>
+        </Marquee>
       </div>
     </div>
   );
--- src/components/strips/TrackStrip.tsx
+++ src/components/strips/TrackStrip.tsx
@@ -1,23 +1,24 @@
 import React from "react";
+import Marquee from "react-fast-marquee";
 
 const TrackStrip = () => {
   return (
     <div className="relative my-20">
       <div className="strips relative text-5xl p-2 font-extrabold">
-        <div className="flex justify-between">
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-        </div>
+        <Marquee className="flex justify-between" direction="right">
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+        </Marquee>
       </div>
       <div className="strips-reverse absolute top-0 w-full left-0 text-5xl -rotate-3 p-2 font-extrabold">
-        <div className="flex justify-between overflow-hidden">
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-          <div className="">TRACK</div>
-        </div>
+        <Marquee className="flex justify-between">
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+          <div className="px-40">TRACK</div>
+        </Marquee>
       </div>
     </div>
   );
